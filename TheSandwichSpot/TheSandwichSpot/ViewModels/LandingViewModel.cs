f59590a4871322cdd476249e7eb50f8e6564c32c
Javier Calderon Jr <javierjcjr@gmail.com>
2020-08-28 22:41:56 -0700

    finished Code

--- TheSandwichSpot/TheSandwichSpot/ViewModels/LandingViewModel.cs
+++ TheSandwichSpot/TheSandwichSpot/ViewModels/LandingViewModel.cs
@@ -57,17 +57,17 @@ namespace TheSandwichSpot.ViewModel
             return new ObservableCollection<Sandwich>
             {
                 new Sandwich { Name = "CLASSIC", Price = 12.99f, Image = "sand3.png", Description = "Dutch Cruch Bread, Ham, Turkey, Lettuce, tomatoes, cheese"},
-                new Sandwich { Name = "DOUBLE", Price = 19.99f, Image = "sand2.png", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"},
-                new Sandwich { Name = "SHARK", Price = 17.29f, Image = "sand4.png", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"},
-                new Sandwich { Name = "CHICKEN", Price = 15.99f, Image = "sand5.png", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"},
-                new Sandwich { Name = "MEAT", Price = 11.99f, Image = "sand6.png", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"},
-                new Sandwich { Name = "BBQ", Price = 13.99f, Image = "sand1.png", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"},
-                new Sandwich { Name = "MODELO", Price = 11.99f, Image = "modelo.png", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"},
-                new Sandwich { Name = "MODELO", Price = 13.99f, Image = "modelob.jpg", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"},
-                new Sandwich { Name = "WHITE CLAW", Price = 11.99f, Image = "whiteclaw.png", Description = "Beer made for Basic Bithces and Simps"},
-                new Sandwich { Name = "CORONA", Price = 13.99f, Image = "corona.jpg", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"},
-                new Sandwich { Name = "BUD LIGHT", Price = 11.99f, Image = "budlight.png", Description = "Lame Cheap beer"},
-                new Sandwich { Name = "MOJITO", Price = 13.99f, Image = "mojito.png", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"}
+                new Sandwich { Name = "DOUBLE", Price = 11.99f, Image = "sand2.png", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"},
+                new Sandwich { Name = "SHARK", Price = 12.29f, Image = "sand4.png", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"},
+                new Sandwich { Name = "CHICKEN", Price = 10.99f, Image = "sand5.png", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"},
+                new Sandwich { Name = "MEAT", Price = 11.99f, Image = "sand6.png", Description = "Big Chunchy Sandwich that will fill you up"},
+                new Sandwich { Name = "BBQ", Price = 9.99f, Image = "sand1.png", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"},
+                new Sandwich { Name = "MODELO", Price = 6.00f, Image = "modelo.png", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"},
+                new Sandwich { Name = "MODELO", Price = 6.50f, Image = "modelob.jpg", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"},
+                new Sandwich { Name = "WHITE CLAW", Price = 6.00f, Image = "whiteclaw.png", Description = "Beer made for Basic Bithces and Basic Bros"},
+                new Sandwich { Name = "CORONA", Price = 6.00f, Image = "corona.jpg", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"},
+                new Sandwich { Name = "BUD LIGHT", Price = 5.00f, Image = "budlight.png", Description = "Lame Cheap beer"},
+                new Sandwich { Name = "MOJITO", Price = 11.00f, Image = "mojito.png", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies"}
             };
         }
     }
